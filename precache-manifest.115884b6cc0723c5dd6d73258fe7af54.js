e67222df0d0ed605fd4bdf004d6078b8069d448a
Furkan YILMAZ <furkan.yilmaz@iyzico.com>
2020-02-13 10:16:24 +0300

    Updates

---
+++ precache-manifest.115884b6cc0723c5dd6d73258fe7af54.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312e3dafe219e5e6c1156d356def3d17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33dbc60684efea1eb6a6",
+    "url": "/static/css/main.b860485c.chunk.css"
+  },
+  {
+    "revision": "e6b569fece3fd5c01e21",
+    "url": "/static/js/2.401fbbd2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.401fbbd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33dbc60684efea1eb6a6",
+    "url": "/static/js/main.133c0199.chunk.js"
+  },
+  {
+    "revision": "c2317f42794b6aecc6f0",
+    "url": "/static/js/runtime-main.dca7bc42.js"
+  },
+  {
+    "revision": "ffd530408006bfab28946d2d81455897",
+    "url": "/static/media/devoxx.ffd53040.jpeg"
+  },
+  {
+    "revision": "9bcd650aecd1ebf4e6a0743d72fcaa91",
+    "url": "/static/media/iyzicoTeamBlack.9bcd650a.jpeg"
+  },
+  {
+    "revision": "6c388ecfc1913a404dccb2f82d24bd38",
+    "url": "/static/media/kadriyePair.6c388ecf.png"
+  }
+]);
